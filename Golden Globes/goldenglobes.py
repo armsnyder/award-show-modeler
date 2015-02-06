f625a5ae1cfd10c18b720a4d0d50699c172cf5f5
friendly-flame <AdamSnyder2016@u.northwestern.edu>
2015-02-06 02:16:13 -0600

    Wrote a processor to find hosts

--- Golden Globes/goldenglobes.py
+++ Golden Globes/goldenglobes.py
@@ -12,7 +12,10 @@
 
 import modules.cmd_line as cmd_line
 from modules.Database import Database
-import modules.util as util
+import re
+import threading
+import modules.process_hosts as process_hosts
+from modules.Result import Result
 
 __author__ = "Kristen Amaddio, Neal Kfoury, Michael Nowakowski, and Adam Snyder"
 __credits__ = ["Kristen Amaddio", "Neal Kfoury", "Michael Nowakowski", "Adam Snyder"]
@@ -29,8 +32,21 @@ def main():
     """
 
     db = Database(cmd_line.args.database, cmd_line.args.collection)
+    result = Result()
     raw_input('Shall we begin execution? ')
-    print 'yay!'
+    process_tweets(db, result)
+    result.print_results()
+
+
+def process_tweets(db, result):
+    """loads tweets into memory and calls helper (multithreaded) functions to process tweets as they arrive"""
+    thread_hosts = threading.Thread(target=process_hosts.process_hosts, args=(db, result))
+    thread_hosts.start()
+    while thread_hosts.is_alive():
+        thread_hosts.join(1)
+        print "Analyzing..."
+    return
+
 
 if __name__ == '__main__':
     main()
