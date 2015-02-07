277adec8b45e944b988b12e9cdc5b3dbafd25949
nakfoury <nakfoury@gmail.com>
2015-02-06 23:44:03 -0600

    categories stub

--- Golden Globes/modules/process_categories.py
+++ Golden Globes/modules/process_categories.py
@@ -1,3 +1,6 @@
 import nltk
 import util
 
+#okay well this is a very critical comment without which my partners
+#will DEFINITELY not be able to complete the project
+#therefore, it had better push
