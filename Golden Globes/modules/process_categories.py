8a680620fea916af0bdf824d3a4a4f74250a6cb9
nakfoury <nakfoury@gmail.com>
2015-02-07 00:07:52 -0600

    0-100

--- Golden Globes/modules/process_categories.py
+++ Golden Globes/modules/process_categories.py
@@ -4,3 +4,9 @@ import util
 #okay well this is a very critical comment without which my partners
 #will DEFINITELY not be able to complete the project
 #therefore, it had better pus
+
+def zinger():
+    a = [0,100]
+    for i in a:
+        print("real quick")
+    return
