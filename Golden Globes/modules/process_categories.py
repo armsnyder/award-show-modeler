15d0631ddb78079bb11e6655e759bb8502fbd328
nakfoury <nakfoury@gmail.com>
2015-02-06 23:59:17 -0600

    deleted h

--- Golden Globes/modules/process_categories.py
+++ Golden Globes/modules/process_categories.py
@@ -3,4 +3,4 @@ import util
 
 #okay well this is a very critical comment without which my partners
 #will DEFINITELY not be able to complete the project
-#therefore, it had better push
+#therefore, it had better pus
