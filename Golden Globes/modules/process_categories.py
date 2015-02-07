2048cb2c6ade3e09a67810703a6e50b9d4a21578
friendly-flame <AdamSnyder2016@u.northwestern.edu>
2015-02-07 00:00:37 -0600

    is cool

--- Golden Globes/modules/process_categories.py
+++ Golden Globes/modules/process_categories.py
@@ -4,3 +4,6 @@ import util
 #okay well this is a very critical comment without which my partners
 #will DEFINITELY not be able to complete the project
 #therefore, it had better pus
+
+def flame(is_a_cool_guy):
+    print is_a_cool_guy
