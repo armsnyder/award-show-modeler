f384306a0e860fa548f8a219484ac9040ab97385
friendly-flame <armsnyder@gmail.com>
2015-02-15 05:12:43 -0600

    Improved process_winners

--- Golden Globes/modules/regex.py
+++ Golden Globes/modules/regex.py
@@ -32,8 +32,15 @@ nominees = re.compile(r'\bnomi?')
 
 #  -- Winners -- #
 
-winners = re.compile(r'(.*)\b(won|wins)(.*)\bfor\b(.*)')
-
+winners = re.compile(r'(?=.*best)\bw[io]n', re.I)
+best_str = r'(best .+?)(?:!|\.|\?|\bon\b|\bfrom|\bfor|\bat)'
+winner_models = [
+    re.compile(r'([@#]\w+) w[io]ns.*' + best_str, re.I),
+    re.compile(r'\bcongrat.*? (?:to )?(.*?)(?:\bon\b|\bfrom|\bfor).*' + best_str, re.I),
+    re.compile(r'[\.\?!:] (.* )w[io]ns.*' + best_str, re.I),
+    re.compile(r'[#@]\w* (.* )w[io]ns.*' + best_str, re.I),
+    re.compile(r'(.* )w[io]ns.*' + best_str, re.I)
+    ]
 
 # -- Obscure -- #
 
