d51340493a1da7fa1cb362f4f6033e8758f6dd81
friendly-flame <AdamSnyder2016@u.northwestern.edu>
2015-02-16 12:12:58 -0600

    Changed names regex to use non-capture groups

--- Golden Globes/modules/regex.py
+++ Golden Globes/modules/regex.py
@@ -10,7 +10,7 @@ def optional_space(text):
     return text.replace(' ', ' ?')
 
 # TODO: generalize names for all shows
-name = re.compile(r'\b(?!Golden|Best)([A-Z]([a-z]+|[.A-Z] ?){1,3} )[A-Z][a-z][A-Z]?[a-z]*')
+name = re.compile(r'\b(?!Golden|Best)(?:[A-Z](?:[a-z]+|[.A-Z] ?){1,3} )[A-Z][a-z][A-Z]?[a-z]*')
 subjunctive = re.compile(r'\bhop[ei]|\bwish|\bwant|\bshould', re.I)
 congrat = re.compile(r'congrats|congratulations', re.I)
 
