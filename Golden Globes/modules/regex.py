82b2af832ddf678e3944a33288428856033c33f7
friendly-flame <AdamSnyder2016@u.northwestern.edu>
2015-02-16 22:26:49 -0600

    Changing best regex

--- Golden Globes/modules/regex.py
+++ Golden Globes/modules/regex.py
@@ -34,7 +34,7 @@ eh = re.compile(r'(if (.*) wins)', re.I)
 #  -- Winners -- #
 
 winners = re.compile(r'(?=.*best)\bw[io]n', re.I)
-best_str = r'([# ]best.+?)(?:!|\.|\?|\bon\b|\bfrom|\bfor|\bat)'
+best_str = r'(\bbest.+?)(?:!|\.|\?|\bon\b|\bfrom|\bfor|\bat)'
 winner_models = [
     re.compile(r'([@#]\w+) w[io]n.*' + best_str, re.I),
     re.compile(r'\bcongrat.*? (?:to )?(.*?) (?:\bon\b|\bfrom|\bfor).*' + best_str, re.I),
